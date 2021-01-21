d122c19f47996d9e2072bad89bbecae3a4cfc47b
ShijuPJohn <shijupjohn4u@gmail.com>
2021-01-21 20:15:03 +0530

    continue implementing action creators

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,10 +1,14 @@
 import {getInitialData} from "../utils/api";
+import {receiveUsers} from "./users";
+import {receiveTweets} from "./tweets";
 
-export function handleInitialData(){
-    return (dispatch)=>{
+const AUTHED_USER='tylermcginnis'
+export function handleInitialData() {
+    return (dispatch) => {
         return getInitialData()
-            .then(({users,tweets})=>{
-
+            .then(({users, tweets}) => {
+                dispatch(receiveUsers(users))
+                dispatch(receiveTweets(tweets))
             })
     }
 }
