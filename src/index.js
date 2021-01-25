89e9b029652a2cefe227918168a1245769e53ecd
ShijuPJohn <shijupjohn4u@gmail.com>
2021-01-25 23:22:02 +0530

    middleware - custom - and combine custom middleware and redux thunk

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
-import {createStore} from "redux";
+import { createStore} from "redux";
 import reducer from './reducers'
 import {Provider} from "react-redux";
+import middleware from './middlewares'
 
-const store = createStore(reducer)
+const store = createStore(reducer, middleware)
 ReactDOM.render(
     <Provider store={store}>
         <App/>
